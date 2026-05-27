19ee59841f4252806cba2e6a501cf3819c4b0dc4
EmillyVN <emilly.nolepa.freitas@escola.pr.gov.br>
2026-05-27 11:36:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const body =document.body;
+body.style.margin=  "0"
+
+//========== CABEÇALHO ==========
+const cabecalho = document.createElement('h1');
+cabecalho.textContent = 'criando elementos com JavaScript';
+
+cabecalho.style.color = '#ffffff';
+cabecalho.style.fontSize = '40px';
+cabecalho.style
